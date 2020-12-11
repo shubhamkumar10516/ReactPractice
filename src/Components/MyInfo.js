50c2eb82b7f1645c2ad21044670e41e025e9aa40
shubham <shubhamkumar10516@gmail.com>
2020-12-11 13:25:39 +0530

    [Shubham]ADDED: Concept of Components and styling added

---
+++ src/Components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+function MyInfo(){
+
+    return(
+        <div className = "info">
+            <h1>Shubham Kumar</h1>
+            <p>I am Full stack developer in Capgemini</p>
+            <ul>
+                <li>Japan</li>
+                <li>Korea</li>
+                <li>Europe</li>
+            </ul>
+        </div>
+    );
+}
+
+export default MyInfo;
